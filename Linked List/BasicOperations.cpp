a94185ae45126ec96d47dfd4659d377df10852e9
shreeyakapoor190 <shreeyakapoor190@gmail.com>
2021-09-24 03:17:47 +0530

    commit changes

---
+++ Linked List/BasicOperations.cpp
@@ -0,0 +1,8 @@
+#include <stdio.h>
+using namespace std;
+// LINKED LIST - memory created dynamically
+// head node stores the add of 1st node
+// head node does not store any data (only the add of 1st node)
+// data type of head = node*
+// 1st node has 2 parts = data of the node + add of 2nd node
+// all the nodes are of the data type defined by us - custom datatype - node
