7ba3bc5e575c9e19eff4db8ba8354a7d1d2740a0
shreeyakapoor190 <shreeyakapoor190@gmail.com>
2022-05-27 15:42:17 +0530

    diameter of tree

--- Tree/Implementation_of_binary_tree.cpp
+++ Tree/Implementation_of_binary_tree.cpp
@@ -47,10 +47,9 @@ int height(Node *root)
     {
         return 0;
     }
-    else
-    {
-        return max(height(root->left), height(root->right)) + 1;
-    }
+    int h1 = height(root->left);
+    int h2 = height(root->right);
+    return 1 + max(h1, h2);
 }
 void levelOrder(Node *root)
 {
@@ -72,6 +71,52 @@ void levelOrder(Node *root)
             q.push(curr->right);
     }
 }
+void levelOrderLinebyLine(Node *root) // more used while solving questions
+{
+    // after every level insert a NULL which will specify the end of one level and start of new level
+    // When we take out a null from the queue, it means that we have reached the end of the current level.
+    // So we now have a new level completely inside the queue and we again push null into the queue to indicate this.
+    if (root == NULL)
+        return;
+    queue<Node *> q;
+    q.push(root);
+    q.push(NULL);
+    while (q.size() > 1)
+    {
+        Node *curr = q.front();
+        q.pop();
+        if (curr == NULL)
+        {
+            cout << "\n";
+            q.push(NULL);
+            continue;
+        }
+        cout << curr->key << " ";
+        if (curr->left)
+            q.push(curr->left);
+        if (curr->right)
+            q.push(curr->right);
+    }
+}
+int diameter(Node *root) // time complexity O(N^2)- at every node we are calculating the height of tree
+{
+    if (!root)
+        return 0;
+    // get height of left and right subtree
+    int lHeight = height(root->left);
+    int rHeight = height(root->right);
+    int currHeight = lHeight + rHeight + 1; // when diameter passes through the root/curr node
+    // therefore left subtree+ right subtree+ the node itself
+
+    // get diameter of left and right subtree
+    int lDiameter = diameter(root->left);  // when diameter lies in the left subtree
+    int rDiameter = diameter(root->right); // when diameter lies in the right subtree
+
+    return max(currHeight, max(lDiameter, rDiameter));
+}
+int diameterOptimized(Node *root) // time complexity O(N)
+{
+}
 int main()
 {
     Node *root = new Node(10);
@@ -90,4 +135,10 @@ int main()
     cout << "height of the tree is: " << height(root) << endl;
     cout << "level order traversal/ breadth first search: " << endl;
     levelOrder(root);
+    cout << endl;
+    cout << "level order line by line: \n";
+    levelOrderLinebyLine(root);
+    cout << endl;
+    cout << "diameter of tree: " << diameter(root) << endl;
+    cout << "diameter of tree: " << diameterOptimized(root) << endl;
 }
